acb2399e962d184fe5e92d16142924bb920573b3
severinnhq <sstierbusiness@gmail.com>
2025-02-06 11:54:47 +0100

    ready

--- components/header.tsx
+++ components/header.tsx
@@ -8,6 +8,7 @@ import { Menu, X, LogOut } from "lucide-react"
 import { useRouter } from "next/navigation"
 
 interface HeaderProps {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   userStatus: string | null;
 }
 
